ada3cb0c18e6222e32b5725d421b7f4536e57742
Adam Šimek <simek.adam@outlook.cz>
2024-04-23 00:13:39 +0200

    Big update

--- code/ansible/opennebula/roles/osd/files/api.py
+++ code/ansible/opennebula/roles/osd/files/api.py
@@ -1,21 +1,15 @@
-import requests
-import json
-import os
-import sys
-import socket
+import requests, json, os, sys, socket, yaml
+
 # username = str(sys.argv[1])
 # password = str(sys.argv[2])
 # ip = str(sys.argv[3])
 # monitor = str(sys.argv[4])
 # hostname = str(sys.argv[5])
-config = json.load(open("config.json"))
+config = yaml.safe_load(open("config.yml"))
 username = str(config["username"])
 password = str(config["password"])
-ip = str(config["ip"])
-monitor = str(config["monitor"])
-hostname = str(socket.gethostname())
 # Get Token
-url = "https://"+monitor+":8443/api/auth"
+url = "https://"+str(config["monitor"])+":8443/api/auth"
 headers = {
     "Accept": "application/vnd.ceph.api.v1.0+json"
 }
@@ -30,23 +24,23 @@ response = requests.post(url, headers=headers, json=data, verify=False)
 values = response.json()
 token = values["token"]
 
-url = "https://"+monitor+":8443/api/osd"
-headers = {
-    "Accept": "application/vnd.ceph.api.v1.0+json",
-    "Authorization": "Bearer " + token
-}
-response = requests.get(url,headers=headers,verify=False)
+# url = "https://"+monitor+":8443/api/osd"
+# headers = {
+#     "Accept": "application/vnd.ceph.api.v1.0+json",
+#     "Authorization": "Bearer " + token
+# }
+# response = requests.get(url,headers=headers,verify=False)
 # print(response.json())
 # print(response.status_code)
 # Add host
-url = "https://"+monitor+":8443/api/host"
+url = "https://"+str(config["monitor"])+":8443/api/host"
 headers = {
     "Accept": "application/vnd.ceph.api.v0.1+json",
     "Authorization": "Bearer " + token
 }
 data = {
-    "addr": ip,
-    "hostname": hostname,
+    "addr": str(config["ip"]),
+    "hostname": str(socket.gethostname()),
     "labels": [
         "osd",
         "rbd"
